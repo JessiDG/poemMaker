e1639e87e8762e85087d14c134b51d19011fc6ac
JessiDG <feelingelephants@gmail.com>
2018-12-07 17:32:05 -0800

    Updated error handling

--- src/sestina.java
+++ src/sestina.java
@@ -4,13 +4,22 @@ import java.util.Scanner;
 
 public class sestina {
 
+    int NUM_STANZAS = 6;
+
     public void sestina(){
         String firstStanza = getFirstStanza();
-        String[] lines = toStringArraySestina(firstStanza);
-        for(int n = 0; n < lines.length; n++)
-            lines[n] = cleanUp(lines[n]);
-        toTxtFile(lines);
-        toConsole(lines);
+
+        try {
+            String[] lines = toStringArraySestina(firstStanza);
+            for(int n = 0; n < lines.length; n++) {
+                lines[n] = cleanUp(lines[n]);
+            }
+            toTxtFile(lines);
+            toConsole(lines);
+        }
+        catch (NullPointerException e){
+            System.out.println("Doesn't look like there's enough text there to make a poem. Try again!");
+        }
     }
 
     private String getFirstStanza(){
@@ -21,20 +30,27 @@ public class sestina {
     }
 
     private String[] toStringArraySestina(String firstStanza){
-        int numStanzas = 6;
-        String[] lines = new String[numStanzas];
+        if(firstStanza.length()<NUM_STANZAS){
+            throw new NullPointerException("toStringArraySestina");
+        }
+
+        String[] lines = new String[NUM_STANZAS + 1];
         int lineCounter = 0;
 
         for (int i = 0; i < firstStanza.length(); i++){
             if(firstStanza.charAt(i) != '\n'){
-
-                if (firstStanza.charAt(i) == '/'){
-                    lineCounter++;
-                    if(lineCounter > 6){
-                        System.out.println(lineCounter + " is too many lines for a sestina");
-                        return null;
+                if(lineCounter < 6) {
+                    if (firstStanza.charAt(i) == '/') {
+                        lineCounter++;
                     }
                 }
+                    else{
+                        System.out.println("Sex means \"six\" in French, so " + (lineCounter + 1)+ " is too many " +
+                                "lines for a sestina, let's stick with the " +
+                                "first "+ (lineCounter) +", ok?");
+                        return lines;
+                    }
+
                 lines[lineCounter] += firstStanza.charAt(i);
             }
         }
@@ -43,15 +59,16 @@ public class sestina {
 
     private void toTxtFile(String[] lines){
         java.util.Date currentDate = Calendar.getInstance().getTime();
-        int numStanzas = lines.length;
 
         try (PrintWriter out = new PrintWriter("sestina."+currentDate+".txt")) {
             out.println("Your sestina:");
 
-            for(int i = 0; i < numStanzas; i++){
+            for(int i = 0; i < NUM_STANZAS - 1; i++){
                 String[] nextStanza = lines.clone();
-                out.println((i+1) + "." + "\n" + nextStanza[0] + "\n" + nextStanza[1] + "\n" +
-                        nextStanza[2] + "\n" + nextStanza[3] + "\n" + nextStanza[4] + "\n" + nextStanza[5] + "\n");
+                out.println((i+1) + "." + "\n" + nextStanza[0] + "\n" +
+                        nextStanza[1] + "\n" + nextStanza[2] + "\n" +
+                        nextStanza[3] + "\n" + nextStanza[4] + "\n" +
+                        nextStanza[5] + "\n");
                 nextStanza[0] = lines[5];
                 nextStanza[1] = lines[0];
                 nextStanza[2] = lines[4];
@@ -62,18 +79,18 @@ public class sestina {
                 lines = nextStanza;
             }
 
-            out.println((numStanzas+1) + "." + "\n" + lines[0] + "\n" + lines[2] + "\n" +
+            out.println((NUM_STANZAS) + "." + "\n" + lines[0] + "\n" + lines[2] + "\n" +
                     lines[4] + "\n" + "\n");
         }
         catch (java.io.FileNotFoundException e){System.out.println("There has been a writing error.");}
     }
 
     private void toConsole(String[] lines){
-        int numStanzas = lines.length;
+        int NUM_STANZAS = lines.length;
         for(int n = 0; n < lines.length; n++)
             lines[n] = cleanUp(lines[n]);
 
-        for(int i = 0; i < numStanzas; i++){
+        for(int i = 0; i < NUM_STANZAS - 1; i++){
             String[] nextStanza = lines.clone();
             System.out.println((i+1) + "." + "\n" + nextStanza[0] + "\n" + nextStanza[1] + "\n" +
                     nextStanza[2] + "\n" + nextStanza[3] + "\n" + nextStanza[4] + "\n" + nextStanza[5] + "\n");
@@ -86,12 +103,12 @@ public class sestina {
 
             lines = nextStanza;
         }
-        System.out.println((numStanzas+1) + "." + "\n" + lines[0] + "\n" + lines[2] + "\n" +
+        System.out.println((NUM_STANZAS) + "." + "\n" + lines[0] + "\n" + lines[2] + "\n" +
                 lines[4] + "\n" + "\n");
     }
 
     private String cleanUp(String line){
-        String[] badPrefices = {" ", "null/ ", "null /", "null"};
+        String[] badPrefices = {" ", "null/ ", "null /", "null", "/"};
         for(String s : badPrefices) {
             if (line.startsWith(s))
                 line = line.substring(s.length());
